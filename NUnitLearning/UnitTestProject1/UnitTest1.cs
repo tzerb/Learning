7b6ca330905cca1bfe6da78c488b8341b6248f5d
Zirbel, Tim <TZirbel@jjkeller.com>
2016-01-04 15:01:52 -0600

    Test runner didn't support nUnit v3

---
+++ NUnitLearning/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace UnitTestProject1
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        [Test]
+        public void TestMethod1()
+        {
+        }
+    }
+}
