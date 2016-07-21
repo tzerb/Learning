12a574a6671b75e5c4b988ae120a4f5e3a7d1a1a
Tim Zirbel <tzerb@hotmail.com>
2016-07-21 14:24:51 -0500

    Add misc temp projects.

---
+++ LearnMaterialUi/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import MyAwesomeReactComponent from './MyAwesomeReactComponent';
+
+const App = () => (
+  <div>Hello</div>
+  );
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('app')
+);
